5e1118cf51fb590ff3c704468575f493ab5c39ff
Aman Kumar <thisisamank@gmail.com>
2024-07-19 18:12:38 +0530

    intial commit

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
---
+++ zsh/.spaceshiprc.zsh
@@ -0,0 +1,12 @@
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ORDER=(
+  time          # Time stampe
+  user          # Username
+  host          # Hostname
+  dir           # Current directory
+  git           # Git repository details
+  hg            # Mercurial repository details
+  jobs          # Background jobs
+  exit_code     # Exit code of the last command
+  char          # Prompt character
+)
